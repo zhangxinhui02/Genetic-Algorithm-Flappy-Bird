04236af9f619c3d9ed3c437daa8b95cf0d87780b
智慧之_光 <i@miku.team>
2024-08-23 00:41:09 +0800

    feat: add README, License and comments.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-simulation_generations = 30
-birds_count_each_generation = 30
-better_k = 2
-change_rate = 0.5
-time_range_sec = 1.5
+simulation_generations = 30  # 要模拟多少代
+birds_count_each_generation = 30  # 每一代有多少个体
+better_k = 2  # 选择优秀个体遗传到下一代的指数
+change_rate = 0.5  # 基因突变概率
+time_range_sec = 1.5  # 跳跃间隔时间上限
